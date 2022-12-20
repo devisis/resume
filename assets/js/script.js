4c8466dc86d901754ab6c075abbde33e5ee8b53c
DevIsis <kitcher96@gmail.com>
2022-12-20 17:33:37 +0000

    Add js test add colour to project side preview

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const projectItem = document.querySelector(".project-item");
+const github = document.querySelectorAll(".fab");
+const site = document.querySelectorAll("fas");
+const glow = document.querySelector(".glow");
+
+projectItem.addEventListener("click", e => {
+    console.log(e.target)
+})
